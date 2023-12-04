9bf5e00684cea2da9815031d26d5709084b2bbb9
lukashermadini <Shermadinil.luka@gmail.com>
2023-12-04 15:41:15 +0300

    serializer update

--- skymarket/users/serializers.py
+++ skymarket/users/serializers.py
@@ -1,21 +1,19 @@
-from djoser.serializers import UserCreateSerializer as BaseUserRegistrationSerializer
-from rest_framework import serializers
+from djoser.serializers import UserCreateSerializer
 from django.contrib.auth import get_user_model
+from rest_framework import serializers
 
 User = get_user_model()
 # TODO Здесь нам придется переопределить сериалайзер, который использует djoser
 # TODO для создания пользователя из за того, что у нас имеются нестандартные поля
 
 
-class UserRegistrationSerializer(BaseUserRegistrationSerializer):
-    def create(self, validated_data):
-        user = User.objects.create(**validated_data)
-
-        user.set_password(validated_data.get('password'))
-        user.save()
-
-        return user
+class UserRegistrationSerializer(UserCreateSerializer):
+    class Meta(UserCreateSerializer.Meta):
+        model = User
+        fields = ("id", "email", "first_name", "last_name", "password", "phone", 'image')
 
 
 class CurrentUserSerializer(serializers.ModelSerializer):
-    pass
+    class Meta:
+        model = User
+        fields = ("id", "email", "first_name", "last_name", "phone", 'image')
